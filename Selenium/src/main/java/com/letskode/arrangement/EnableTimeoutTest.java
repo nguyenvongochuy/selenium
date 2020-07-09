64964e9fa732f15afc99135f2de2090482b42c69
nguyenvongochuy <nguyenvongochuy@gmail.com>
2020-07-09 17:26:05 -0500

    add reserve order

--- Selenium/src/main/java/com/letskode/arrangement/EnableTimeoutTest.java
+++ Selenium/src/main/java/com/letskode/arrangement/EnableTimeoutTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class DependentTest {
+public class EnableTimeoutTest {
     /*public static final String webUrl = "https://letskodeit.teachable.com/p/practice";
     private String driverPath = "resources/chromedriver-v83.exe"; // windows driver for chrome
     //private String driverPath = "resources/chromedriver"; // linux driver for chrome
@@ -31,31 +31,19 @@ public class DependentTest {
         System.out.println("After class");
     }
 
-    //assume test1 depend on test2, test3 depend on test 1
-    //--> test2 -> test1 -> test3 (test4 any random)
-    @Test(dependsOnMethods = {"test2"}, alwaysRun = true)  //allow test1 run even test2 failed, will not ignore
-    public void test1() throws InterruptedException {
+
+    @Test(enabled = false)  //not allow to run
+    public void test1() {
         System.out.println("test1");
 
     }
 
-    @Test
+    @Test(timeOut = 100)  //fail if run longer than 100ms
     public void test2() throws InterruptedException {
         System.out.println("test2");
-        int num = 2+1;
-        Assert.assertEquals(2, num);
+        Thread.sleep(200);
     }
 
-    @Test(dependsOnMethods = {"test1"})
-    public void test3() throws InterruptedException {
-        System.out.println("test3");
 
-    }
-
-    @Test
-    public void test4() throws InterruptedException {
-        System.out.println("test4");
-
-    }
 
 }
--- Selenium/src/main/java/com/letskode/arrangement/Preserve1Test.java
+++ Selenium/src/main/java/com/letskode/arrangement/Preserve1Test.java
@@ -1,11 +1,10 @@
 package com.letskode.arrangement;
 
-import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class DependentTest {
+public class Preserve1Test {
     /*public static final String webUrl = "https://letskodeit.teachable.com/p/practice";
     private String driverPath = "resources/chromedriver-v83.exe"; // windows driver for chrome
     //private String driverPath = "resources/chromedriver"; // linux driver for chrome
@@ -31,31 +30,18 @@ public class DependentTest {
         System.out.println("After class");
     }
 
-    //assume test1 depend on test2, test3 depend on test 1
-    //--> test2 -> test1 -> test3 (test4 any random)
-    @Test(dependsOnMethods = {"test2"}, alwaysRun = true)  //allow test1 run even test2 failed, will not ignore
-    public void test1() throws InterruptedException {
-        System.out.println("test1");
+
+    @Test
+    public void preserve1Test1() {
+        System.out.println("preserve1Test1");
 
     }
 
     @Test
-    public void test2() throws InterruptedException {
-        System.out.println("test2");
-        int num = 2+1;
-        Assert.assertEquals(2, num);
+    public void preserve1Test2() {
+        System.out.println("preserve1Test2");
     }
 
-    @Test(dependsOnMethods = {"test1"})
-    public void test3() throws InterruptedException {
-        System.out.println("test3");
 
-    }
-
-    @Test
-    public void test4() throws InterruptedException {
-        System.out.println("test4");
-
-    }
 
 }
