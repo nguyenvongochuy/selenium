42cd8b43bdd3cc3c49fa54750379467d8a63c8bf
nguyenvongochuy <nguyenvongochuy@gmail.com>
2020-08-31 01:17:55 -0500

    changed

--- Selenium/src/main/java/com/utils/Utilities.java
+++ Selenium/src/main/java/com/utils/Utilities.java
@@ -1,12 +1,14 @@
 package com.utils;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Utilities {
+
     public static By getByFromWebElement(WebElement element) {
         By by = null;
         String[] pathVariables = (element.toString().split("->")[1].replaceFirst("(?s)(.*)\\]", "$1" + "")).split(":");
@@ -48,9 +50,42 @@ public class Utilities {
         return by;
     }
 
-    public static void explicitWaitByWebElement(WebDriver driver, WebElement element) {
+    public static void explicitWaitByWebElementvisibilityOfElementLocated(WebDriver driver, WebElement element) {
         WebDriverWait wait = new WebDriverWait(driver,20);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(
-                Utilities.getByFromWebElement(element)));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Utilities.getByFromWebElement(element)));
     }
+    
+    public static void explicitWaitByWebElementvisibilityOfElementLocated(WebDriver driver, By by) {
+        WebDriverWait wait = new WebDriverWait(driver,20);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+    
+    public static void explicitWaitByElementToBeClickable(WebDriver driver, WebElement element) {
+        WebDriverWait wait = new WebDriverWait(driver,20);
+        wait.until(ExpectedConditions.elementToBeClickable(Utilities.getByFromWebElement(element)));
+    }
+    
+    public static void explicitWaitByElementToBeClickable(WebDriver driver, By by) {
+        WebDriverWait wait = new WebDriverWait(driver,20);
+        wait.until(ExpectedConditions.elementToBeClickable(by));
+    }
+    
+    public static void explicitWaitForAlert(WebDriver driver) {
+    	WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.alertIsPresent());
+    }
+    
+    /**
+     * Some error happen when click on button image, with this error:
+     * Selenium Web Driver & Java. Element is not clickable at point (x, y). Other element would receive the click
+     * --> Should use this function
+     * @param driver
+     * @param element
+     */    
+    public static void clickByJavascript(WebDriver driver, WebElement element) {
+    	JavascriptExecutor executor = (JavascriptExecutor) driver;
+    	executor.executeScript("arguments[0].click()", element);
+    }
+    
 }
+
